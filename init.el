6fe0ba749870ac611f211345899609551a0fd6d3
Arman Tarkhanian <armantarkhanian@gmail.com>
2021-09-02 21:26:42 +0300

    auto-commit

--- init.el
+++ init.el
@@ -16,10 +16,10 @@
 (setq warning-minimum-level
       :emergency)
 
-(global-set-key (kbd "S-<up>") 'windmove-up)
-(global-set-key (kbd "S-<down>") 'windmove-down)
-(global-set-key (kbd "S-<left>") 'windmove-left)
-(global-set-key (kbd "S-<right>") 'windmove-right)
+;; (global-set-key (kbd "S-<up>") 'windmove-up)
+;; (global-set-key (kbd "S-<down>") 'windmove-down)
+;; (global-set-key (kbd "S-<left>") 'windmove-left)
+;; (global-set-key (kbd "S-<right>") 'windmove-right)
 
 (defun calc-region (point mark)
     (interactive "r")
@@ -28,6 +28,12 @@
                    point
                    mark)))
     (insert (concat " = " result)))
+
+(use-package key-chord
+    :ensure t
+    :config
+    (key-chord-define-global "fj" 'ace-jump-char-mode)
+    )
 
 (use-package
     ibuffer-sidebar
@@ -144,8 +150,16 @@
     ace-jump-mode
     :ensure t)
 
-(global-set-key (kbd "M-m") 'ace-jump-char-mode)
-(global-set-key (kbd "M-o") 'next-window-any-frame)
+(defun switchNextBuffer()
+    (interactive)
+    (switch-to-next-buffer)
+    (setq name (buffer-name))
+    (if (equal "*" (substring name 0 1))
+        (switchNextBuffer)
+        )
+    )
+
+(global-set-key (kbd "M-o") 'switchNextBuffer)
 
 (use-package
     ace-window
@@ -509,7 +523,7 @@ This command does not push text to `kill-ring'."
 
 (global-set-key (kbd "C-M-p") 'backwardParagraph)
 (global-set-key (kbd "C-M-n") 'forwardParagraph)
-(global-set-key (kbd "C-M-/") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'comment-or-uncomment-region)
 (global-set-key (kbd "C-j") 'lsp-execute-code-action)
 (global-set-key (kbd "C-x C-g") 'goto-line-preview)
 (global-set-key (kbd "C-r") 'replace-string)
@@ -679,7 +693,7 @@ This command does not push text to `kill-ring'."
    ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
  '(cursor-type '(bar . 2))
  '(custom-safe-themes
-   '("303cfaa6ce6653d3299583f9f002107487860b701d314fff589b7df77263d5fd" "98ef70f6028d10db0a3e1178b741df39c2dfefad97eda6ff1f17d5e83fe32179" "1d44ec8ec6ec6e6be32f2f73edf398620bb721afeed50f75df6b12ccff0fbb15" "f4c8f0b999a6407211a899401315a628e1a5ae2f408c04a33b14d7aa3ed86187" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default))
+   '("2f1ed0c737c498295c7b72396cb9c168556b7bfc637967ec2fba044b63d55ece" "9c45a0351e89fe6e950ed7ff7c7bf785344dcb56598e6d97532c638c90000309" "665fbb8e59d19f13bb1f1215af8180758b464eefe239b9a59ccd57486958b68b" "62eca1d5615ff9695691704030914c470e6a6c0244d5049893715bcbd202c0dd" "8543bb312515c9e477f629a729c2bf8a4f71332c5756f32f3cb5fbbd0a0a785a" "303cfaa6ce6653d3299583f9f002107487860b701d314fff589b7df77263d5fd" "98ef70f6028d10db0a3e1178b741df39c2dfefad97eda6ff1f17d5e83fe32179" "1d44ec8ec6ec6e6be32f2f73edf398620bb721afeed50f75df6b12ccff0fbb15" "f4c8f0b999a6407211a899401315a628e1a5ae2f408c04a33b14d7aa3ed86187" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default))
  '(display-time-day-and-date nil)
  '(exwm-floating-border-color "#121212")
  '(fci-rule-color "#515151")
@@ -693,7 +707,7 @@ This command does not push text to `kill-ring'."
  '(nil nil t)
  '(objed-cursor-color "#FF5E5E")
  '(package-selected-packages
-   '(ace-window rainbow-mode elisp-format vscdark-theme yasnippet-snippets yaml-mode wttrin webpaste web-mode vue-mode vscode-dark-plus-theme vs-light-theme use-package tao-theme tango-plus-theme tabbar spacemacs-theme smartparens selected restclient protobuf-mode projectile nord-theme neotree mwim multiple-cursors multi-web-mode mark-multiple magit lsp-ui lsp-python-ms light-soap-theme json-reformat jetbrains-darcula-theme intellij-theme ibuffer-sidebar highlight-indentation highlight-indent-guides goto-line-preview google-translate google go-mode github-theme github-modern-theme git format-all flycheck-golangci-lint flatui-theme expand-region espresso-theme epc dumb-jump doom-themes doom-modeline dashboard company centaur-tabs bm block-nav avy atom-one-dark-theme all-the-icons-dired afternoon-theme ace-jump-mode))
+   '(key-chord ace-window rainbow-mode elisp-format vscdark-theme yasnippet-snippets yaml-mode wttrin webpaste web-mode vue-mode vscode-dark-plus-theme vs-light-theme use-package tao-theme tango-plus-theme tabbar spacemacs-theme smartparens selected restclient protobuf-mode projectile nord-theme neotree mwim multiple-cursors multi-web-mode mark-multiple magit lsp-ui lsp-python-ms light-soap-theme json-reformat jetbrains-darcula-theme intellij-theme ibuffer-sidebar highlight-indentation highlight-indent-guides goto-line-preview google-translate google go-mode github-theme github-modern-theme git format-all flycheck-golangci-lint flatui-theme expand-region espresso-theme epc dumb-jump doom-themes doom-modeline dashboard company centaur-tabs bm block-nav avy atom-one-dark-theme all-the-icons-dired afternoon-theme ace-jump-mode))
  '(pdf-view-midnight-colors (cons "#d4d4d4" "#191919"))
  '(rustic-ansi-faces
    ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
