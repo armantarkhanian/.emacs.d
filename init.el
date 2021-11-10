3edceb6ba06feb44cc4cf69f591af58580198e7f
Arman Tarkhanian <armantarkhanian@gmail.com>
2021-11-10 12:36:10 +0300

    auto commit

--- init.el
+++ init.el
@@ -21,9 +21,6 @@
 ;; (global-set-key (kbd "S-<left>") 'windmove-left)
 ;; (global-set-key (kbd "S-<right>") 'windmove-right)
 
-(global-set-key (kbd "M-.") 'lsp-goto-type-definition)
-(global-set-key (kbd "C-M-.") 'lsp-goto-implementation)
-
 (defun calc-region (point mark)
     (interactive "r")
     (setq result (calc-eval
