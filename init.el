a750c5a3fd58d6fe68b57be30989d604797e32c7
Arman Tarkhanian <armantarkhanian@gmail.com>
2021-11-23 09:34:11 +0300

    auto commit

--- init.el
+++ init.el
@@ -16,12 +16,7 @@
 (setq warning-minimum-level
       :emergency)
 
-;; (global-set-key (kbd "S-<up>") 'windmove-up)
-;; (global-set-key (kbd "S-<down>") 'windmove-down)
-;; (global-set-key (kbd "S-<left>") 'windmove-left)
-;; (global-set-key (kbd "S-<right>") 'windmove-right)
-
-(global-set-key (kbd "C-M-.") 'lsp-goto-implementation)
+(load "~/.emacs.d/keybindings.el")
 
 (defun calc-region (point mark)
     (interactive "r")
@@ -70,7 +65,7 @@
 
 (key-chord-mode 1)
 
-(global-set-key (kbd "C-M-j") 'ace-jump-char-mode)
+
 
 (use-package
     typescript-mode
@@ -100,7 +95,7 @@
     multiple-cursors
     :ensure t)
 
-(global-set-key (kbd "C->") 'mark-next-like-this)
+
 
 (use-package
     bm
@@ -187,7 +182,7 @@
     (if (equal "*" (substring name 0 1))
         (switchNextBuffer)))
 
-(global-set-key (kbd "M-o") 'switchNextBuffer)
+
 
 (use-package
     ace-window
@@ -352,17 +347,15 @@
 (setq inhibit-splash-screen   t)
 (setq ingibit-startup-message t)
 (setq echo-keystrokes 0.1 use-dialog-box nil visible-bell t)
-(global-set-key (kbd "RET") 'newline-and-indent)
-(global-set-key (kbd "C-j") 'newline-and-indent)
 
-(global-set-key (kbd "C-<up>") 'scroll-down-line)
-(global-set-key (kbd "C-<down>") 'scroll-up-line)
-(global-set-key (kbd "C-<right>") 'flycheck-next-error)
-(global-set-key (kbd "C-<left>") 'flycheck-previous-error)
 
-(global-set-key (kbd "C-<tab>") 'buffer-menu)
-;; (global-set-key (kbd "C-<tab>") 'switch-to-next-buffer)
-;; (global-set-key (kbd "C-<tab> backspace") 'switch-to-prev-buffer)
+
+
+
+
+
+
+
 
 (defvar --backup-directory "~/.emacs.d/backups")
 (if (not (file-exists-p --backup-directory))
@@ -385,13 +378,12 @@
 
 (add-hook 'prog-mode-hook #'hs-minor-mode)
 
-(global-set-key (kbd "C--") 'hs-hide-block)
-(global-set-key (kbd "C-=") 'hs-show-block)
+
+
 
 (require 'imenu)
 (setq imenu-auto-rescan      t)
 (setq imenu-use-popup-menu nil)
-(global-set-key (kbd "<f6>") 'imenu)
 (setq frame-title-format "GNU Emacs: %b")
 (setq inhibit-splash-screen   t)
 (setq ingibit-startup-message t)
@@ -454,8 +446,6 @@
 (setq scroll-conservatively 10000)
 (defalias 'yes-or-no-p 'y-or-n-p)
 (setq x-select-enable-clipboard t)
-;;(global-set-key (kbd "C-x b") 'buffer-menu)
-;;(global-set-key (kbd "C-x C-b") 'buffer-menu)
 
 (defun gotoUp ()
     (interactive)
@@ -521,12 +511,12 @@ This command does not push text to `kill-ring'."
     (interactive)
     (re-search-forward "[{}]" nil t 1))
 
-(global-set-key (kbd "C-<backspace>") 'backward-delete-word)
-(global-set-key (kbd "C-k") 'custom-kill-line)
-(global-set-key (kbd "M-<backspace>") 'backward-delete-word)
-(global-set-key (kbd "M-d") 'forward-delete-word)
-(global-set-key (kbd "M-p") 'backward-page)
-(global-set-key (kbd "M-n") 'forward-page)
+
+
+
+
+
+
 
 (defun backwardParagraph()
     (interactive)
@@ -549,12 +539,9 @@ This command does not push text to `kill-ring'."
     (next-line)
     (next-line))
 
-(global-set-key (kbd "C-M-p") 'backwardParagraph)
-(global-set-key (kbd "C-M-n") 'forwardParagraph)
-(global-set-key (kbd "M-/") 'comment-or-uncomment-region)
-(global-set-key (kbd "C-j") 'lsp-execute-code-action)
-(global-set-key (kbd "C-x C-g") 'goto-line-preview)
-(global-set-key (kbd "C-r") 'replace-string)
+
+
+
 
 (use-package
     flycheck
@@ -726,8 +713,7 @@ This command does not push text to `kill-ring'."
  '(ansi-color-names-vector
    ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
  '(custom-safe-themes
-   (quote
-    ("cf861f5603b7d22cb3545a7c63b2ee424c34d8ed3b3aa52d13abfea4765cffe7" "2f1ed0c737c498295c7b72396cb9c168556b7bfc637967ec2fba044b63d55ece" "9c45a0351e89fe6e950ed7ff7c7bf785344dcb56598e6d97532c638c90000309" "665fbb8e59d19f13bb1f1215af8180758b464eefe239b9a59ccd57486958b68b" "62eca1d5615ff9695691704030914c470e6a6c0244d5049893715bcbd202c0dd" "8543bb312515c9e477f629a729c2bf8a4f71332c5756f32f3cb5fbbd0a0a785a" "303cfaa6ce6653d3299583f9f002107487860b701d314fff589b7df77263d5fd" "98ef70f6028d10db0a3e1178b741df39c2dfefad97eda6ff1f17d5e83fe32179" "1d44ec8ec6ec6e6be32f2f73edf398620bb721afeed50f75df6b12ccff0fbb15" "f4c8f0b999a6407211a899401315a628e1a5ae2f408c04a33b14d7aa3ed86187" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default)))
+   '("cf861f5603b7d22cb3545a7c63b2ee424c34d8ed3b3aa52d13abfea4765cffe7" "2f1ed0c737c498295c7b72396cb9c168556b7bfc637967ec2fba044b63d55ece" "9c45a0351e89fe6e950ed7ff7c7bf785344dcb56598e6d97532c638c90000309" "665fbb8e59d19f13bb1f1215af8180758b464eefe239b9a59ccd57486958b68b" "62eca1d5615ff9695691704030914c470e6a6c0244d5049893715bcbd202c0dd" "8543bb312515c9e477f629a729c2bf8a4f71332c5756f32f3cb5fbbd0a0a785a" "303cfaa6ce6653d3299583f9f002107487860b701d314fff589b7df77263d5fd" "98ef70f6028d10db0a3e1178b741df39c2dfefad97eda6ff1f17d5e83fe32179" "1d44ec8ec6ec6e6be32f2f73edf398620bb721afeed50f75df6b12ccff0fbb15" "f4c8f0b999a6407211a899401315a628e1a5ae2f408c04a33b14d7aa3ed86187" "a9a67b318b7417adbedaab02f05fa679973e9718d9d26075c6235b1f0db703c8" default))
  '(display-time-day-and-date nil)
  '(exwm-floating-border-color "#121212")
  '(fci-rule-color "#515151")
@@ -736,13 +722,12 @@ This command does not push text to `kill-ring'."
  '(jdee-db-active-breakpoint-face-colors (cons "#171F24" "#FFFFFF"))
  '(jdee-db-requested-breakpoint-face-colors (cons "#171F24" "#468800"))
  '(jdee-db-spec-breakpoint-face-colors (cons "#171F24" "#777778"))
- '(linum-format (quote dynamic))
+ '(linum-format 'dynamic)
  '(lsp-eldoc-enable-hover nil)
  '(nil nil t)
  '(objed-cursor-color "#FF5E5E")
  '(package-selected-packages
-   (quote
-    (graphql-mode dap-mode go-dlv indent-tools sql-sqlline sqlup-mode sql-smie format-sql reformatter sql-indent sqlformat html-to-markdown typescript-mode dockerfile-mode key-chord ace-window rainbow-mode elisp-format vscdark-theme yasnippet-snippets yaml-mode wttrin webpaste web-mode vue-mode vscode-dark-plus-theme vs-light-theme use-package tao-theme tango-plus-theme tabbar spacemacs-theme smartparens selected restclient protobuf-mode projectile nord-theme neotree mwim multiple-cursors multi-web-mode mark-multiple magit lsp-ui lsp-python-ms light-soap-theme json-reformat jetbrains-darcula-theme intellij-theme ibuffer-sidebar highlight-indentation highlight-indent-guides goto-line-preview google-translate google go-mode github-theme github-modern-theme git format-all flycheck-golangci-lint flatui-theme expand-region espresso-theme epc dumb-jump doom-themes doom-modeline dashboard company centaur-tabs bm block-nav avy atom-one-dark-theme all-the-icons-dired afternoon-theme ace-jump-mode)))
+   '(graphql-mode dap-mode go-dlv indent-tools sql-sqlline sqlup-mode sql-smie format-sql reformatter sql-indent sqlformat html-to-markdown typescript-mode dockerfile-mode key-chord ace-window rainbow-mode elisp-format vscdark-theme yasnippet-snippets yaml-mode wttrin webpaste web-mode vue-mode vscode-dark-plus-theme vs-light-theme use-package tao-theme tango-plus-theme tabbar spacemacs-theme smartparens selected restclient protobuf-mode projectile nord-theme neotree mwim multiple-cursors multi-web-mode mark-multiple magit lsp-ui lsp-python-ms light-soap-theme json-reformat jetbrains-darcula-theme intellij-theme ibuffer-sidebar highlight-indentation highlight-indent-guides goto-line-preview google-translate google go-mode github-theme github-modern-theme git format-all flycheck-golangci-lint flatui-theme expand-region espresso-theme epc dumb-jump doom-themes doom-modeline dashboard company centaur-tabs bm block-nav avy atom-one-dark-theme all-the-icons-dired afternoon-theme ace-jump-mode))
  '(pdf-view-midnight-colors (cons "#d4d4d4" "#191919"))
  '(rustic-ansi-faces
    ["#191919" "#FF5E5E" "#468800" "#E9FDAC" "#8CDAFF" "#C586C0" "#85DDFF" "#d4d4d4"])
