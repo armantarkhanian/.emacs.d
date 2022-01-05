bd2d6a363f0d66ad0f102e907304afc93294a40e
Arman Tarkhanian <armantarkhanian@gmail.com>
2022-01-05 14:18:35 +0300

    auto commit

--- init_window.el
+++ init_window.el
@@ -560,7 +560,25 @@
 
 (use-package
     flycheck
-    :ensure t)
+    :ensure t
+	:diminish flycheck-mode
+	:config
+	(global-flycheck-mode)
+	;; disable jshint since we prefer eslint checking
+	(setq-default flycheck-disabled-checkers
+                  (append flycheck-disabled-checkers
+                          '(javascript-jshint)))
+
+	(setq flycheck-checkers '(javascript-eslint))
+	;; use eslint with web-mode for jsx files
+	(flycheck-add-mode 'javascript-eslint 'web-mode)
+	(flycheck-add-mode 'javascript-eslint 'js2-mode)
+	(flycheck-add-mode 'javascript-eslint 'js-mode)
+	;; disable json-jsonlist checking for json files
+	(setq-default flycheck-disabled-checkers
+                  (append flycheck-disabled-checkers
+                          '(json-jsonlist)))
+	)
 
 (use-package yasnippet
 	:ensure t
