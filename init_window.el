a552dae9095a00a76d1f1281415d39538f5a53d3
Arman Tarkhanian <armantarkhanian@gmail.com>
2022-06-15 21:38:18 +0300

    auto commit

--- init_window.el
+++ init_window.el
@@ -8,22 +8,7 @@
 
 (setq warning-minimum-level :emergency)
 
-(use-package
-	dired-sidebar
-	:ensure t
-	:init
-	(add-hook 'dired-sidebar-mode-hook
-              (lambda ()
-				  (unless (file-remote-p default-directory)
-					  (auto-revert-mode))))
-	:config
-	(push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
-	(push 'rotate-windows dired-sidebar-toggle-hidden-commands)
-
-	(setq dired-sidebar-subtree-line-prefix "__")
-	(setq dired-sidebar-theme 'vscode)
-	(setq dired-sidebar-use-term-integration t)
-	(setq dired-sidebar-use-custom-font t))
+(use-package nginx-mode :ensure t)
 
 (global-so-long-mode 1)
 (toggle-truncate-lines 1)
