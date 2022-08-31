ee2d93a87755e8325919064ece9dd5108f06613b
Arman Tarkhanian <armantarkhanian@gmail.com>
2022-08-31 09:49:39 +0300

    auto commit

--- init_window.el
+++ init_window.el
@@ -448,6 +448,20 @@
 
 (require 'lsp-java)
 
+    ;;;; This snippet enables lua-mode
+;; This line is not necessary, if lua-mode.el is already on your load-path
+(add-to-list 'load-path "/home/arman/.emacs.d/elpa/lua-mode/")
+
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+(add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
+(add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+
+(setq lua-indent-level 2)
+(setq lua-indent-nested-block-content-align nil)
+
+(add-hook 'lua-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
+
 (use-package
 	lsp-mode
 	:ensure t
